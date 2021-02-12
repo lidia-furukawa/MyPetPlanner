66191a484731522a2e9b1d3e1c25df0217eefbc9
Lidia-Furukawa <lidia.furukawa@gmail.com>
2021-02-12 14:56:54 -0300

    Initial Commit

---
+++ MyPetPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPetPlanner
+//
+//  Created by Lidia on 12/02/21.
+//  Copyright © 2021 LidiaF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyPetPlannerTests/MyPetPlannerTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyPetPlannerTests.swift
+//  MyPetPlannerTests
+//
+//  Created by Lidia on 12/02/21.
+//  Copyright © 2021 LidiaF. All rights reserved.
+//
+
+import XCTest
+@testable import MyPetPlanner
+
+class MyPetPlannerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
