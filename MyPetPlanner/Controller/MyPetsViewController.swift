d3a45a27c474af846a798d61dddccf930c2edae8
Lidia-Furukawa <lidia.furukawa@gmail.com>
2021-03-03 19:25:52 -0300

    Add a Fetched Results Controller to MyPetsVC

--- MyPetPlanner/Controller/MyPetsViewController.swift
+++ MyPetPlanner/Controller/MyPetsViewController.swift
@@ -11,11 +11,40 @@ import CoreData
 
 class MyPetsViewController: UIViewController {
 
-    var dataController:DataController!
+    var dataController: DataController!
     
+    var fetchedResultsController: NSFetchedResultsController<Pet>!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupFetchedResultsController()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setupFetchedResultsController()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        fetchedResultsController = nil
+    }
+    
+    func setupFetchedResultsController() {
+        let fetchRequest:NSFetchRequest<Pet> = Pet.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
+    }
 }
+
