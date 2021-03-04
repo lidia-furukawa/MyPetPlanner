324d86961da238cd74a8c7b7a337b2c888fe1d8e
Lidia-Furukawa <lidia.furukawa@gmail.com>
2021-03-04 11:38:36 -0300

    Add a Fetched Results Controller to NewPetVC

--- MyPetPlanner/Controller/NewPetViewController.swift
+++ MyPetPlanner/Controller/NewPetViewController.swift
@@ -11,10 +11,13 @@ import CoreData
 
 class NewPetViewController: UIViewController {
 
+    @IBOutlet weak var saveButton: UIBarButtonItem!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var nameTextField: UITextField!
    
     var dataController: DataController!
+
+    var fetchedResultsController: NSFetchedResultsController<Pet>!
 
     /// The pet whose infos will be added
     var newPet: Pet!
@@ -22,7 +25,34 @@ class NewPetViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        setupFetchedResultsController()
+        saveButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setupFetchedResultsController()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        fetchedResultsController = nil
+    }
+    
+    func setupFetchedResultsController() {
+        let fetchRequest:NSFetchRequest<Pet> = Pet.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
     }
     
     @IBAction func saveButton(_ sender: Any) {
