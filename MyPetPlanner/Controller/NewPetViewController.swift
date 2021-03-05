577be8111bf4f469f8b629d85f88c4e0dbce3e90
Lidia-Furukawa <lidia.furukawa@gmail.com>
2021-03-05 11:41:08 -0300

    Initial implement of NewPetVC fields (textfields, controls, pickers)

--- MyPetPlanner/Controller/NewPetViewController.swift
+++ MyPetPlanner/Controller/NewPetViewController.swift
@@ -13,8 +13,18 @@ class NewPetViewController: UIViewController {
 
     @IBOutlet weak var saveButton: UIBarButtonItem!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var basicInformationLabel: UILabel!
+    @IBOutlet weak var bodyMeasurementsLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
-   
+    @IBOutlet weak var typeControl: UISegmentedControl!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var genderControl: UISegmentedControl!
+    @IBOutlet weak var breedTextField: UITextField!
+    @IBOutlet weak var colorTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet var textFields: [UITextField]!
+    
     var dataController: DataController!
 
     var fetchedResultsController: NSFetchedResultsController<Pet>!
@@ -22,11 +32,43 @@ class NewPetViewController: UIViewController {
     /// The pet whose infos will be added
     var newPet: Pet!
     
+    let pickerView = UIPickerView()
+    
+    var dogBreeds: [String] = []
+    
+    var catBreeds: [String] = []
+    
+    let dateFormatter = DateFormatter()
+    
+    fileprivate func roundLabelEdges(label: UILabel, cornerRadius: CGFloat) {
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = cornerRadius
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupFetchedResultsController()
+        roundLabelEdges(label: basicInformationLabel, cornerRadius: 5)
+        roundLabelEdges(label: bodyMeasurementsLabel, cornerRadius: 5)
+        
+        dateFormatter.dateFormat = "MM-dd-yyyy"
+        birthdayTextField.text = dateFormatter.string(from: Date())
         saveButton.isEnabled = false
+        
+        for textField in textFields {
+            textField.delegate = self
+        }
+
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        DogAPIClient.getBreedsList(completion: handleBreedsListResponse(breeds:error:))
+
+        NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: nameTextField, queue: .main) { notif in
+            if let text = self.nameTextField.text, !text.isEmpty {
+                self.saveButton.isEnabled = true
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -55,12 +97,126 @@ class NewPetViewController: UIViewController {
         }
     }
     
+    @objc func handleDatePicker(_ sender: UIDatePicker!) {
+        dateFormatter.dateFormat = "MM-dd-yyyy"
+        birthdayTextField.text = dateFormatter.string(from: sender.date)
+    }
+    
+    func handleBreedsListResponse(breeds: [String], error: Error?) {
+        self.dogBreeds = breeds
+        DispatchQueue.main.async {
+            self.pickerView.reloadAllComponents()
+        }
+    }
+    
+    func updatePicker(){
+        self.pickerView.reloadAllComponents()
+    }
+    
     @IBAction func saveButton(_ sender: Any) {
+        let pet = Pet(context: dataController.viewContext)
+        pet.name = nameTextField.text
+        let selectedType = typeControl.selectedSegmentIndex
+        pet.type = typeControl.titleForSegment(at: selectedType)
         
+        if let birthdayText = birthdayTextField.text {
+            pet.birthday = dateFormatter.date(from: birthdayText)
+        }
+        
+        pet.breed = breedTextField.text
+        pet.color = colorTextField.text
+        
+        let selectedGender = genderControl.selectedSegmentIndex
+        pet.gender = genderControl.titleForSegment(at: selectedGender)
+        
+        if let weightText = weightTextField.text {
+            pet.weight = Double(weightText) ?? 0
+        }
+        
+        if let heightText = heightTextField.text {
+            pet.height = Double(heightText) ?? 0
+        }
+
+        try? dataController.viewContext.save()
     }
     
     @IBAction func cancelButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
+}
+
+// -----------------------------------------------------------------------------
+// MARK: - UITextFieldDelegate
+
+extension NewPetViewController: UITextFieldDelegate {
+    
+    /// Make the next textField the first responder when the user taps the return key
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case nameTextField:
+            birthdayTextField.becomeFirstResponder()
+        case birthdayTextField:
+            breedTextField.becomeFirstResponder()
+        case breedTextField:
+            colorTextField.becomeFirstResponder()
+        case colorTextField:
+            weightTextField.becomeFirstResponder()
+        case weightTextField:
+            heightTextField.becomeFirstResponder()
+        default:
+            heightTextField.resignFirstResponder()
+        }
+        return true
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        switch textField {
+        case birthdayTextField:
+            let datePickerView = UIDatePicker()
+            datePickerView.datePickerMode = .date
+            datePickerView.backgroundColor = .white
+            birthdayTextField.inputView = datePickerView
+            datePickerView.addTarget(self, action: #selector(handleDatePicker(_:)), for: .valueChanged)
+        case breedTextField:
+            pickerView.reloadAllComponents()
+            pickerView.backgroundColor = .white
+            breedTextField.inputView = pickerView
+        default:
+            break
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+// MARK: - UIPickerViewDataSource, UIPickerViewDelegate
+
+extension NewPetViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if typeControl.isEnabledForSegment(at: 0) {
+            return dogBreeds.count
+        } else {
+            return catBreeds.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if typeControl.isEnabledForSegment(at: 0) {
+            return dogBreeds[row]
+        } else {
+            return catBreeds[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if typeControl.isEnabledForSegment(at: 0) {
+            breedTextField.text = dogBreeds[row]
+        } else {
+            breedTextField.text = catBreeds[row]
+        }
+    }
 }
