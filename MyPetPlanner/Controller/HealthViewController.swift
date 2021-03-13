ef6b096e7bf8466e51b1889420cd6eee44708352
Lidia-Furukawa <lidia.furukawa@gmail.com>
2021-03-13 18:43:13 -0300

    Initial implement of HealthVC table view's datasource and delegate methods

--- MyPetPlanner/Controller/HealthViewController.swift
+++ MyPetPlanner/Controller/HealthViewController.swift
@@ -7,13 +7,73 @@
 //
 
 import UIKit
+import CoreData
 
 class HealthViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dataController: DataController!
+    
+    /// The pet passed by `MyPetsViewController` when a pet cell's selected
+    var pet: Pet?
+    
+    let sectionTitles = ["Food", "Grooming", "Parasite Control", "Medication"]
+    let sectionDataSource = [["Kibble/Dry Food", "Fresh/Raw Food"], ["Bathing", "Brushing/Clipping", "Teeth", "Nails", "Ears"], ["Internal", "External"], ["Medication", "Supplements"]]
+    
+    let tintColor = #colorLiteral(red: 0.6509035826, green: 0.2576052547, blue: 0.8440084457, alpha: 1)
+    let backgroundColor = #colorLiteral(red: 0.8941176471, green: 0.7176470588, blue: 0.8980392157, alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.tableFooterView = UIView()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.title = "Selected Pet: \(pet?.name ?? "None")"
+    }
+}
 
+// -----------------------------------------------------------------------------
+// MARK: - UITableViewDataSource, UITableViewDelegate
+
+extension HealthViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sectionDataSource[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        (view as! UITableViewHeaderFooterView).contentView.backgroundColor = backgroundColor
+//        (view as! UITableViewHeaderFooterView).textLabel?.textColor = UIColor.white
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.tintColor = tintColor
+        cell.accessoryType = .disclosureIndicator
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let reuseIdentifier = "HealthCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier)!
+        
+        cell.textLabel?.text = sectionDataSource[indexPath.section][indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+
+    }
 }
