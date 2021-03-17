2ee69e67b57808e465a6f487ff79a32ed4c71b61
Lidia-Furukawa <lidia.furukawa@gmail.com>
2021-03-17 17:19:12 -0300

    Create FoodVC and sketch UI elements

---
+++ MyPetPlanner/Controller/FoodViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FoodViewController.swift
+//  MyPetPlanner
+//
+//  Created by Lidia on 17/03/21.
+//  Copyright © 2021 LidiaF. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class FoodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
