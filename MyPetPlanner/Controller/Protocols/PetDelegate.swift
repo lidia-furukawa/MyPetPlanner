30c6eb675b1f315a3222dfba0cae61114733dd22
Lidia-Furukawa <lidia.furukawa@gmail.com>
2021-04-01 11:57:52 -0300

    Create a Pet delegate protocol to delegate pet selection

---
+++ MyPetPlanner/Controller/Protocols/PetDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  PetDelegate.swift
+//  MyPetPlanner
+//
+//  Created by Lidia on 31/03/21.
+//  Copyright © 2021 LidiaF. All rights reserved.
+//
+
+protocol PetDelegate: class {
+    func petWasSelected(pet: Pet)
+}
