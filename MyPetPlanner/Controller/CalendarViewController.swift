a9f5680a9c50b55f567bacb547035fbc34d9bf82
Lidia-Furukawa <lidia.furukawa@gmail.com>
2021-04-14 15:30:18 -0300

    Fix the delete reminder functionality

--- MyPetPlanner/Controller/CalendarViewController.swift
+++ MyPetPlanner/Controller/CalendarViewController.swift
@@ -16,25 +16,30 @@ class CalendarViewController: UIViewController {
     var eventStore = EKEventStore()
     var reminders: [EKReminder]?
     let calendarKey = "MyPetPlanner"
+    var selectedReminder: EKReminder!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initView()
         checkAuthorizationStatus(for: .reminder)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         checkAuthorizationStatus(for: .reminder)
-        
         subscribeToEventStoreNotifications()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         unsubscribeFromNotifications()
     }
 
+    func initView() {
+        navigationItem.title = "My Pets Reminders"
+        tableView.tableFooterView = UIView()
+    }
+    
     func loadEntity(_ type: EKEntityType) {
         // Use an Event Store instance to create and properly configure an NSPredicate
         if let calendar = EKCalendar.loadCalendar(type: .reminder, from: eventStore, with: calendarKey) {
@@ -54,6 +59,18 @@ class CalendarViewController: UIViewController {
             }
         } else {
             print("No reminders to show")
+        }
+    }
+    
+    func deleteReminder(at indexPath: IndexPath) {
+        if let reminder = reminders?[indexPath.row] {
+            do {
+                try self.eventStore.remove(reminder, commit: true)
+                self.reminders?.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            } catch {
+                fatalError("Error deleting the reminder")
+            }
         }
     }
 }
@@ -96,16 +113,17 @@ extension CalendarViewController: TrailingSwipeActions {
     }
     
     func setDeleteAction(at indexPath: IndexPath) {
-        if let reminders = self.reminders {
-            let reminder = reminders[indexPath.row]
-            do {
-                try eventStore.remove(reminder, commit: true)
-                self.reminders?.remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
-            } catch {
-                fatalError("Error deleting the reminder")
-            }
-        }
+        let deleteAlert = AlertInformation(
+            title: "Are you sure you want to delete this reminder?",
+            message: "This action cannot be undone",
+            actions: [
+                Action(buttonTitle: "Cancel", buttonStyle: .cancel, handler: nil),
+                Action(buttonTitle: "Delete", buttonStyle: .destructive, handler: {
+                    self.deleteReminder(at: indexPath)
+                })
+            ]
+        )
+        presentAlertDialog(with: deleteAlert)
     }
 }
 
@@ -114,23 +132,19 @@ extension CalendarViewController: TrailingSwipeActions {
 
 extension CalendarViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let reminders = self.reminders {
-            return reminders.count
-        }
-        return 0
+        return reminders?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CalendarCell")!
         
-        if let reminders = self.reminders {
-            let reminder = reminders[indexPath.row]
+        if let reminder = reminders?[indexPath.row] {
             cell.textLabel?.text = reminder.notes
-            
             let dueDate = reminder.dueDateComponents?.date
-            cell.detailTextLabel?.text = "Due date: \(dueDate!.stringFormat))"
-            
-            cell.imageView?.image = UIImage(named: reminder.title)
+            cell.detailTextLabel?.text = "Due date: \(dueDate?.stringFormat ?? "")"
+            let reminderImage = UIImage(named: reminder.title)
+            let templateImage = reminderImage?.withRenderingMode(.alwaysTemplate)
+            cell.imageView?.image = templateImage
         } else {
             cell.textLabel?.text = "Unknown Reminder"
             cell.detailTextLabel?.text = "Unknown Due Date"
