d3a0fd3c564c409689a27b41d61bac4d37a0ca5d
Lidia-Furukawa <lidia.furukawa@gmail.com>
2021-04-19 12:38:58 -0300

    Connect Grooming storyboard outlets to GroomingVC

--- MyPetPlanner/Controller/GroomingViewController.swift
+++ MyPetPlanner/Controller/GroomingViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class GroomingViewController: UIViewController {
 
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var navigationBar: UINavigationBar!
+    @IBOutlet var cancelButton: UIBarButtonItem!
+    @IBOutlet var saveButton: UIBarButtonItem!
+    @IBOutlet var groomingImageView: UIImageView!
+    @IBOutlet var groomingTextField: UITextField!
+    @IBOutlet var frequencyTextField: UITextField!
+    @IBOutlet var frequencyStepper: UIStepper!
+    @IBOutlet var frequencyControl: UISegmentedControl!
+    @IBOutlet var priceTextField: UITextField!
+    @IBOutlet var expensesDateTextField: UITextField!
+    @IBOutlet var expensesSwitch: UISwitch!
+    @IBOutlet var startDateTextField: UITextField!
+    @IBOutlet var endDateTextField: UITextField!
+    @IBOutlet var calendarSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
